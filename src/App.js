b73d83c4cf20ac0d896d54e004e5336e31e7ad77
Jose <pjose14.jp38@yahoo.com>
2020-02-23 21:41:14 -0600

    setup api with axios.characters are displaying. adding some styling with reactstrap as i go

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+// import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+// import EpisodeList from "./components/EpisodeList";
+// import LocationList from "./components/LocationList";
+// import { Container } from "reactstrap";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      {/* <Route exact path="/" component={WelcomePage} /> */}
+      <Route path="/characters" component={CharacterList} />
+      {/* <Route exact path="/episodes" component={EpisodeList} /> */}
+      {/* <Route exact path="/locations" component={LocationList} /> */}
     </main>
   );
 }
